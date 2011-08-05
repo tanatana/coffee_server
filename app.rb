0020b518a6e8f19ffc4cd94a9e405b2f4f3b5f85
tanatana <tugusuke@gmail.com>
2011-08-06 02:17:54 +0900

    offインターフェース実装

--- app.rb
+++ app.rb
@@ -2,12 +2,23 @@
 require 'rubygems'
 require 'sinatra'
 
-
 get '/' do
   "this is a coffee server!"
 end
 
-
 get '/on' do
-  
+  "coffee server will be start to make coffee."
 end
+
+get '/off' do
+  `/bin/Input.exe 0`
+end
+
+get '/warmUp' do
+  "warming up..."
+end
+
+get '' do
+  ""
+end
+
